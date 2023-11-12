7a84e80dbefe02d8fce022beff95569ef6aa2da2
jramilm <malatejramil@gmail.com>
2023-11-12 17:42:30 +0800

    minor change

--- scripts/data_init/load_data.php
+++ scripts/data_init/load_data.php
@@ -1,19 +1,10 @@
 <?php
 require 'vendor/autoload.php';
+require('config/config.php');
+require('config/db.php');
+global $conn;
 
-$f = Faker\Factory::create('en_PH');
-$serverName = "localhost";
-$userName = "root";
-$dbName = 'records_app';
-$pass = '';
-
-$db_conn = mysqli_connect($serverName, $userName, $pass, $dbName);
-
-if (!$db_conn) {
-    'Connection Failed: ' . die(mysqli_connect_error());
-}
-
-function addEmployeesRow(int $limit, $db_conn) : void {
+function addFakeEmployeesRow(int $limit, $conn) : void {
     global $f;
 
     for($i = 0; $i < $limit; $i++) {
@@ -22,19 +13,19 @@ function addEmployeesRow(int $limit, $db_conn) : void {
         $officeID = $f -> numberBetween(1, 50);
         $address = $f -> address;
 
-        $stmt = $db_conn -> prepare("INSERT INTO employee (lastName, firstName, officeID, address) VALUES (?, ?, ?, ?)");
+        $stmt = $conn -> prepare("INSERT INTO employee (lastname, firstname, office_id, address) VALUES (?, ?, ?, ?)");
         $stmt -> bind_param("ssis", $lastName, $firstName, $officeID, $address);
 
         if ($stmt -> execute()) {
             echo "New record created successfully";
         } else {
-            echo "Error: " . mysqli_error($db_conn);
+            echo "Error: " . mysqli_error($conn);
         }
         echo "\n";
     }
 }
 
-function addOfficeRow(int $limit, $db_conn) : void {
+function addFakeOfficeRow(int $limit, $conn) : void {
     global $f;
 
     for($i = 0; $i < $limit; $i++) {
@@ -46,19 +37,19 @@ function addOfficeRow(int $limit, $db_conn) : void {
         $country = $f -> country;
         $postal = $f -> postcode;
 
-        $stmt = $db_conn -> prepare("INSERT INTO office (name, contactnum, email, address, city, country, postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt = $conn -> prepare("INSERT INTO office (name, contactnum, email, address, city, country, postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
         $stmt -> bind_param("sssssss", $name, $contactNum, $email, $address, $city, $country, $postal);
 
         if ($stmt -> execute()) {
             echo "New record created successfully";
         } else {
-            echo "Error: " . mysqli_error($db_conn);
+            echo "Error: " . mysqli_error($conn);
         }
         echo "\n";
     }
 }
 
-function addTransactionRow(int $limit, $db_conn) : void {
+function addFakeTransactionRow(int $limit, $conn) : void {
     global $f;
 
     for($i = 0; $i < $limit; $i++) {
@@ -69,14 +60,14 @@ function addTransactionRow(int $limit, $db_conn) : void {
         $remarks = $f -> optional(0.7) -> word;
         $documentCode = $f -> countryCode;
 
-        $stmt = $db_conn -> prepare("INSERT INTO transaction(employee_id, office_id, datelog, action, remarks, documentcode)
-                                            VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt = $conn -> prepare("INSERT INTO transaction(employee_id, office_id, datelog, action, remarks, documentcode)
+                                  VALUES (?, ?, ?, ?, ?, ?)");
         $stmt -> bind_param("iissss", $employeeID, $officeID, $date, $action, $remarks, $documentCode);
         $stmt -> execute();
     }
     echo "$limit transaction records created successfully.";
 }
 
-addOfficeRow(50, $db_conn);
-addEmployeesRow(200, $db_conn);
-addTransactionRow(500, $db_conn);
+addFakeOfficeRow(50, $conn);
+addFakeEmployeesRow(200, $conn);
+addFakeTransactionRow(500, $conn);
